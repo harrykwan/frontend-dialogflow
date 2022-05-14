fa257f28b66c2e9c0b4c89a405a61cfc82ca1278
harrykwan <harry1998kwan@gmail.com>
2022-05-14 17:47:29 +0800

    auto detect

--- sketch.js
+++ sketch.js
@@ -2,87 +2,70 @@ let mic, recorder, soundFile;
 let state = 0;
 
 function setup() {
-  let cnv = createCanvas(500, 100);
-  cnv.mousePressed(canvasPressed);
-  background(220);
-  textAlign(CENTER, CENTER);
-  soundFormats("mp3");
   // create an audio in
-  mic = new p5.AudioIn();
 
-  // prompts user to enable their browser mic
+  frameRate(4);
+  mic = new p5.AudioIn();
   mic.start();
 
-  // create a sound recorder
-  recorder = new p5.SoundRecorder();
+  userStartAudio();
+  // prompts user to enable their browser mic
 
   fft = new p5.FFT();
   fft.setInput(mic);
-
-  // connect the mic to the recorder
-  recorder.setInput(mic);
-
-  // this sound file will be used to
-  // playback & save the recording
-  soundFile = new p5.SoundFile();
-
-  text("tap to record", width / 2, height / 2);
 }
 
-function draw() {
-  let spectrum = fft.analyze();
-  // console.log(spectrum);
-}
-
-function canvasPressed() {
-  // ensure audio is enabled
+function startlistening() {
   userStartAudio();
+}
 
-  // make sure user enabled the mic
-  if (state === 0 && mic.enabled) {
-    // record to our p5.SoundFile
-    recorder.record(soundFile);
-
-    background(255, 0, 0);
-    text("Recording!", width / 2, height / 2);
-    state++;
-  } else if (state === 1) {
-    background(0, 255, 0);
-
-    // stop recorder and
-    // send result to soundFile
-    recorder.stop();
-
-    text("Play", width / 2, height / 2, 0);
-    state++;
-  } else if (state === 2) {
-    soundFile.play(); // play the result!
-    // save(soundFile, "mySound.wav");
-    const soundBlob = soundFile.getBlob();
-
-    // let serverUrl = "http://localhost:8080/api/agent/voice-input";
-    // let httpRequestOptions = {
-    //   method: "POST",
-    //   body: new FormData().append("", soundBlob),
-    //   headers: new Headers({
-    //     "Content-Type": "multipart/form-data",
-    //   }),
-    // };
-    // httpDo(serverUrl, httpRequestOptions);
-
-    // var formdata = new FormData();
-    // formdata.append("", soundBlob, "mySound.wav");
-
-    // var requestOptions = {
-    //   method: "POST",
-    //   body: formdata,
-    //   redirect: "follow",
-    // };
-
-    // fetch("http://localhost:8080/api/agent/voice-input", requestOptions)
-    //   .then((response) => response.text())
-    //   .then((result) => console.log(result))
-    //   .catch((error) => console.log("error", error));
-    // state++;
+var prevfft;
+var talking = false;
+function draw() {
+  let nowfft = fft.analyze();
+  if (!prevfft) {
+    prevfft = nowfft;
+    return;
   }
+  // let peakDetect = new p5.PeakDetect();
+  // let amplitude = new p5.Amplitude();
+  // console.log(peakDetect, amplitude);
+
+  let fftdiff = [];
+
+  for (var j = 0; j < 9; j++) {
+    let tempresult = 0;
+    for (var k = 0; k < 128; k++) {
+      if ([j * 128 + k] < nowfft.length)
+        tempresult += nowfft[j * 128 + k] - prevfft[j * 128 + k];
+    }
+    fftdiff.push(tempresult);
+  }
+
+  let talkingrate =
+    (abs(fftdiff[1]) +
+      abs(fftdiff[2]) +
+      abs(fftdiff[3] * 1.2) +
+      abs(fftdiff[4] * 1.3) +
+      abs(fftdiff[5] * 1.5)) /
+    5;
+  // console.log(talkingrate);
+
+  if (talkingrate > 100) {
+    if (!talking) {
+      talking = true;
+      console.log("start talking");
+      document.getElementById("start-recording").click();
+    }
+  } else if (talkingrate < 10) {
+    if (talking) {
+      talking = false;
+      console.log("end talking");
+      document.getElementById("stop-recording").click();
+    }
+  }
+
+  // console.log(fftdiff);
+
+  prevfft = nowfft;
 }
