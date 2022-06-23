e4962a9ef39988458b6f21bd4cb05f8ed1e3d297
harrykwan <harry1998kwan@gmail.com>
2022-06-23 19:17:54 +0800

    update

--- public/sketch.js
+++ public/sketch.js
@@ -1,6 +1,5 @@
 let mic, recorder, soundFile;
 let state = 0;
-
 let talkingratelimit = 100;
 
 function setup() {
@@ -23,8 +22,16 @@ function startlistening() {
 var prevfft;
 var talking = false;
 var starttalkingrate = 0;
-
+var talkingaverage = null;
+let noise = 0;
+let noiseprev = [];
+let noiseaveragecount = 20;
+let nowsnr = 1;
+let mytimer = 0;
 function draw() {
+  mytimer += 1;
+  mytimer = mytimer % 4;
+  // console.log(mytimer);
   if (document.getElementById("micsensitivityinput")) {
     talkingratelimit =
       -10 *
@@ -53,25 +60,67 @@ function draw() {
 
   let talkingrate =
     (abs(fftdiff[1]) +
-      abs(fftdiff[2]) +
+      abs(fftdiff[2] * 1.2) +
       abs(fftdiff[3] * 1.2) +
-      abs(fftdiff[4] * 1.3) +
-      abs(fftdiff[5] * 1.5)) /
+      abs(fftdiff[4] * 1) +
+      abs(fftdiff[5] * 1)) /
     5;
-  // console.log(talkingrate);
 
-  if (talkingrate > talkingratelimit) {
+  let talkingvolumn =
+    (nowfft[1] + nowfft[2] * 1.2 + nowfft[3] * 1.2 + nowfft[4] + nowfft[5]) / 5;
+
+  // let talkingvolumn = 0;
+  // nowfft.map((x) => {
+  //   talkingvolumn += x;
+  // });
+  // console.log(talkingrate);
+  if (!talkingaverage) {
+    talkingaverage = talkingvolumn;
+  } else {
+    talkingaverage = (talkingaverage + talkingvolumn) / 2;
+  }
+
+  // console.log("talkingaverage: " + talkingaverage);
+
+  // if (!noise) noise = talkingvolumn;
+  // else {
+  if (noiseprev.length < noiseaveragecount) {
+    noiseprev.push(talkingvolumn);
+  } else {
     if (!talking) {
-      starttalkingrate = talkingrate;
-      talking = true;
-      console.log("start talking");
-      document.getElementById("start-recording").click();
+      noise = 0;
+      noiseprev.map((x) => (noise += x));
+      noise = noise / noiseprev.length;
+      noiseprev.shift();
     }
-  } else if (talkingrate < starttalkingrate / 10) {
-    if (talking) {
-      talking = false;
-      console.log("end talking");
-      document.getElementById("stop-recording").click();
+  }
+  // }
+
+  // console.log("noise: " + noise);
+
+  let snr = noise > 0 ? talkingaverage / noise : 1;
+
+  // console.log(snr * 100 + " " + nowsnr * 100);
+  // console.log("closing? " + abs(snr - nowsnr) * 100);
+  // console.log(talkingrate);
+  // console.log("gate? " + snr * 10);
+  if (noise && talkingaverage && talkingrate) {
+    if (talkingrate > talkingratelimit) {
+      // if (snr > 100) {
+      if (!talking) {
+        nowsnr = snr;
+        starttalkingrate = talkingrate;
+        talking = true;
+        console.log("start talking");
+        document.getElementById("start-recording").click();
+      }
+      // } else if (talkingrate < starttalkingrate / 10) {
+    } else if (abs(snr - nowsnr) * 100 < nowsnr * 10 && mytimer) {
+      if (talking) {
+        talking = false;
+        console.log("end talking");
+        document.getElementById("stop-recording").click();
+      }
     }
   }
 
